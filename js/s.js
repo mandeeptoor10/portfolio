636b926052fd5d644e6509f70c4c56c4eff6de08
Mandeep Toor <97710257+mandeeptoor10@users.noreply.github.com>
2023-07-03 21:05:18 -0400

    Update s.js

--- js/s.js
+++ js/s.js
@@ -98,3 +98,71 @@ function setActiveMenuItem() {
   // Example: Attach the submitForm function to the form's submit event
   var contactForm = document.getElementById("contactForm");
   contactForm.addEventListener("submit", submitForm);
+// JavaScript code for the data analysis portfolio
+
+// Function to display a greeting message in the console
+function greet() {
+  console.log("Welcome to the Data Analysis Portfolio!");
+}
+
+// Function to submit the contact form using AJAX
+function submitForm(event) {
+  event.preventDefault();
+
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var messageInput = document.getElementById("message");
+  var nameError = document.getElementById("nameError");
+  var emailError = document.getElementById("emailError");
+  var messageError = document.getElementById("messageError");
+
+  var isValid = true;
+
+  if (nameInput.value.trim() === "") {
+    nameError.textContent = "Name is required";
+    isValid = false;
+  } else {
+    nameError.textContent = "";
+  }
+
+  if (emailInput.value.trim() === "") {
+    emailError.textContent = "Email is required";
+    isValid = false;
+  } else {
+    emailError.textContent = "";
+  }
+
+  if (messageInput.value.trim() === "") {
+    messageError.textContent = "Message is required";
+    isValid = false;
+  } else {
+    messageError.textContent = "";
+  }
+
+  if (isValid) {
+    var formData = new FormData();
+    formData.append("name", nameInput.value);
+    formData.append("email", emailInput.value);
+    formData.append("message", messageInput.value);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "process.php", true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Process the response from the PHP file
+        console.log(xhr.responseText);
+        // You can update the UI or perform other actions based on the response
+      }
+    };
+
+    xhr.send(formData);
+  }
+}
+
+// Call the greet function on page load
+window.addEventListener("DOMContentLoaded", greet);
+
+// Example: Attach the submitForm function to the form's submit event
+var contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", submitForm);
